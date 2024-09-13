671247edd66e50d7130f52fe222aa095f4cd0dfc
Galih Arizza <test@test>
2024-09-13 13:54:34 +0700

    fixing error from HTML-validator

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Galih Arizza",
   description: "Social Links Profile",
+  viewport: "width=device-width, initial-scale=1",
+  themeColor: "#1f1f1f",
 };
 
 export default function RootLayout({
