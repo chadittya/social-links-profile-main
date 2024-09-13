1a98bff4f9f1f2d5a1e82709a4187415e8cf32be
Galih Arizza <test@test>
2024-09-13 13:45:28 +0700

    fix unescaped entities in your JSX

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -13,7 +13,7 @@ const Profile = () => {
       />
       <h1 className="text-white text-2xl font-bold mb-1">Jessica Randall</h1>
       <p className="text-[#c5f82a] text-sm mb-6">London, United Kingdom</p>
-      <p className="text-white text-sm mb-6">"Front-end developer and avid reader."</p>
+      <p className="text-white text-sm mb-6">{'Front-end developer and avid reader.'}</p>
       <div className="space-y-4">
         <SocialLink href="#" text="GitHub" />
         <SocialLink href="#" text="Frontend Mentor" />
